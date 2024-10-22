3ca2e6aeef9dd075b284ed07c8f511e04a942a9b
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-10-22 23:49:06 +0900

    create new schemes and a Theme

---
+++ Brevis/Settings/Theme.swift
@@ -0,0 +1,44 @@
+//
+//  Theme.swift
+//  Brevis
+//
+//  Created by joe on 10/22/24.
+//
+
+import SwiftUI
+
+#if os(iOS)
+struct Theme {
+    static let font: Font = .headline
+    static let fontSecondary: Font = .body
+    static let fontWeight: Font.Weight = .regular
+    static let modifierFont: Font = .headline
+    static let frameWidth: CGFloat? = nil // for mac
+    static let frameHeight: CGFloat? = nil // for mac
+    static let listStyle = DefaultListStyle()
+    static let sectionHeaderFont: Font = .body
+    static let hotkeyWidth: CGFloat? = nil // for mac
+    static let subtitleColor: Color = .red
+    
+    func layout(isIPad: Bool) -> AnyLayout {
+        isIPad ? AnyLayout(VStackLayout(alignment: .leading)) : AnyLayout(HStackLayout())
+    }
+}
+#else
+struct Theme {
+    static let font: Font = .title
+    static let fontSecondary: Font = .title
+    static let fontWeight: Font.Weight = .semibold
+    static let modifierFont: Font = .title
+    static let frameWidth: CGFloat? = 900 // for mac
+    static let frameHeight: CGFloat? = 1000 // for mac
+    static let listStyle = SidebarListStyle()
+    static let sectionHeaderFont: Font = .largeTitle
+    static let hotkeyWidth: CGFloat? = 200 // for mac
+    static let subtitleColor: Color = .red
+    
+    func layout(isIPad: Bool) -> AnyLayout {
+        AnyLayout(HStackLayout())
+    }
+}
+#endif
